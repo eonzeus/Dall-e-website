9edf679d4f10ae48fa8bd265f0d2b5506093ad3d
Anarchy <anarchy.flawless@gmail.com>
2023-03-04 21:15:20 -0500

    adding search and grid components

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -8,13 +8,23 @@ const Home = () => {
 
   return (
     <section className="max-w-7xl mx-auto">
-      ;
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">
           The Community Showcase
         </h1>
+        <p className="mt-2 text-[#666e75]text-[16px] max-w [500px]">
+          Browse through a collection of imaginative creations by DALL-E
+        </p>
       </div>
-      ;
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </section>
   );
 };
