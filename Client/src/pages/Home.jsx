b23f5cc3b7e6f8d356f2d87e1a1ed49606a3e202
Anarchy <anarchy.flawless@gmail.com>
2023-03-05 21:18:24 -0500

    create post page 2

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Form } from "react-router-dom";
 import { Loader, Card, FormField } from "../components";
+{
+  /*Render cards or posts in the grid we created this seperate component to avoid repitition */
+}
+const RenderCards = ({ data, title }) => {
+  if (data?.lenght > 0) {
+    return;
+    data.map((post) => <Card key={post._id} {...post} />);
+  }
 
+  return (
+    <h1 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h1>
+  );
+};
+
+{
+  /* Main component or */
+}
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
-
+  const [searchText, setSearchText] = useState("");
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -16,13 +32,32 @@ const Home = () => {
           Browse through a collection of imaginative creations by DALL-E
         </p>
       </div>
+
+      <div className="mt-16">
+        <FormField />
+      </div>
+      {/*loading icon*/}
       <div className="mt-10">
         {loading ? (
           <div className="flex justify-center items-center">
             <Loader />
           </div>
         ) : (
-          <></>
+          <>
+            {searchText && (
+              <h2 className="font-medium text-[#666e75] text-xl mb-3">
+                Showing results for
+                <span className="text-[#222328]">{searchText}</span>
+              </h2>
+            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards data={[]} title="No results found" />
+              ) : (
+                <RenderCards data={[]} title="No posts found" />
+              )}
+            </div>
+          </>
         )}
       </div>
     </section>
